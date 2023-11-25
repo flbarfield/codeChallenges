0903ae3102d08fb6b928a3cab63c0234e5a987ab
Ricky Barfield <barfieldfredrick@gmail.com>
2023-11-25 09:51:48 -0700

    morning kyu 6

---
+++ codeWars/6 Kyu/digitalRoot.js
@@ -0,0 +1,55 @@
+// parameter - recieving a number
+// pseudocode:   
+// 1) split number into individual digits
+// 2) add each digit reducing them into a single digit.
+// 3) if digit has two or more digits, continue to reduce until you get a single digit
+
+function digitalRoot(n) {
+    // convert num to string for split method, to seperate units
+    let arr = n.toString().split('')
+    // convert string units back to int
+    arr = (arr.map(Number))
+    // adding arr units into a single sum
+    let dRoot = arr.reduce((acc, currentValue) => {
+      return(acc + currentValue)
+    }, 0)
+    //check if sum has more than 1 digit by converting to string for the check...If num has more than one digit, convert back to num for reducing. A while loop is used to continue the operation if condition isn't met
+    if (dRoot.toString().length > 1) {
+      while (dRoot.toString().length > 1) {
+        dRoot = dRoot.toString().split('')
+        dRoot = dRoot.map(Number)
+        dRoot = dRoot.reduce((acc, currentValue) => {
+            return(acc + currentValue)
+          }, 0)
+      }
+      console.log(dRoot)
+    } else {
+      console.log(dRoot)
+    }
+  }
+  
+  
+  digitalRoot(648199)
+
+  // OTHER SOLUTIONS:
+  
+//   function digital_root(n) {
+//     return (n - 1) % 9 + 1;
+//   }
+
+//   function digital_root(n) {
+//     if (n < 10) return n;
+    
+//     return digital_root(
+//       n.toString().split('').reduce(function(acc, d) { return acc + +d; }, 0));
+//   }
+
+//   function digital_root(n) {
+//     if (n < 10)
+//       return n;
+  
+//     for (var sum = 0, i = 0, n = String(n); i < n.length; i++)
+//       sum += Number(n[i]);
+     
+//     return digital_root(sum);
+//   }
