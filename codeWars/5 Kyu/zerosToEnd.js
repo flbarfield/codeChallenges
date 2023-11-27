e67de41a8a66ef2da5b08ec0ee996d6471cbf142
Ricky Barfield <barfieldfredrick@gmail.com>
2023-11-26 20:01:41 -0700

    successfully completed the problem 5th kyu

--- codeWars/5 Kyu/zerosToEnd.js
+++ codeWars/5 Kyu/zerosToEnd.js
@@ -12,30 +12,59 @@
 // 3) filter out all 0s
 // 4) push 0s up to the counted number
 
-// I'm currently doing exactly what the problem is asking with all sorts of numbers and letters, undefined, and empty values. But I'm not sure at all on what sort of wierd edge case it's failing on via codewars' invisible testing suite. May have to unlock solutions because I'm stumped.
-
 function moveZeros(arr) {
-    let zeroCount = 0
-    let loopCount = 0
-    // loop through array, count number of 0s
-    for (i in arr) {
-      if (arr[i] === 0) {
-        zeroCount ++
-      }
+  console.log(arr)
+  let zeroCount = 0
+  let loopCount = 0
+  // loop through array, count number of 0s
+  for (i in arr) {
+    if (arr[i] === 0) {
+      zeroCount++
     }
-    // filter out all 0s
-    arr.filter((value, index) => {
-      if (value === 0) {
-        arr.splice(index, 1)
-      }
-    })
-    // push 0s up to the counted number
-    while (loopCount < zeroCount) {
-      arr.push(0)
-      loopCount ++
-    }
-    console.log(arr)
   }
+  // filter out all 0s. Put this into a function so I can recursively call it. Ends recursion when there's no more 0s
+  function zeroCheck() {
+    if (arr.includes(0)) {
+      arr.filter((value, index) => {
+        if (value === 0) {
+          arr.splice(index, 1)
+        }
+      })
+    } else {
+      return
+    }
+    zeroCheck()
+  }
+
+  zeroCheck()
+
+  // push 0s up to the counted number
+  while (loopCount < zeroCount) {
+    arr.push(0)
+    loopCount++
+  }
+  console.log(arr)
+}
   
   moveZeros([1,2,0,1,0,1,0,3,0,1])
 
+
+// Whew, brute forced the heck out of that one... Other solutions:
+//
+// ***
+//var moveZeros = function (arr) {
+//   return arr.filter(function(x) {return x !== 0}).concat(arr.filter(function(x) {return x === 0;}));
+// }
+//
+// var moveZeros = function (arr) {
+//   var filtedList = arr.filter(function (num){return num !== 0;});
+//   var zeroList = arr.filter(function (num){return num === 0;});
+//   return filtedList.concat(zeroList);
+// }
+//
+// var moveZeros = function (arr) {
+//   return [
+//     ...(arr.filter(n => n !== 0)),
+//     ...(arr.filter(n => n === 0))
+//   ];
+// }
